7163b34772295eb52d3d0abbd52720f4995d2b91
krypticallusion <krypticallusion@gmail com>
2018-04-20 16:42:13 +0000

    strings.py : Cut it up
    Signed-off-by: krypticallusion <krypticallusion@gmail com>

--- tg_bot/modules/translations/strings.py
+++ tg_bot/modules/translations/strings.py
@@ -4,11 +4,9 @@ from tg_bot.modules.translations.Dutch import DutchStrings
 
 def tld(chat_id, t):
     LANGUAGE = prev_locale(chat_id)
-    if LANGUAGE == 'German' and t in GermanStrings:
+    if LANGUAGE == 'Dutch' and t in DutchStrings:
+        return DutchStrings[t]
+    elif LANGUAGE == 'German' and t in GermanStrings:
         return GermanStrings[t]
     else:
         return t
-    if LANGUAGE == 'Dutch' and t in DutchStrings:
-        return DutchStrings[t]
-    else:
-       return t
