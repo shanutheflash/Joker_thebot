074ed35ac2ffb5a03ead743426a4a863ad4cbff4
krypticallusion <krypticallusion@gmail com>
2018-04-21 07:25:16 +0000

    Add support for Indonesian
    Signed-off-by: krypticallusion <krypticallusion@gmail com>

--- tg_bot/modules/translations/strings.py
+++ tg_bot/modules/translations/strings.py
@@ -1,6 +1,7 @@
 from tg_bot.modules.sql.translation import prev_locale
 from tg_bot.modules.translations.German import GermanStrings
 from tg_bot.modules.translations.Dutch import DutchStrings
+from tg_bot.modules.translations.Indonesian import IndonesianStrings
 
 def tld(chat_id, t):
     LANGUAGE = prev_locale(chat_id)
@@ -8,5 +9,7 @@ def tld(chat_id, t):
         return DutchStrings[t]
     elif LANGUAGE in ('deutsch', 'german') and t in GermanStrings:
         return GermanStrings[t]
+    elif LANGUAGE in ('indonesian') and t in IndonesianStrings:
+        return IndonesianStrings[t]
     else:
         return t
